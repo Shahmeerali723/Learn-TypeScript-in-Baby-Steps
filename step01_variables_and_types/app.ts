b5dd1e56295bd24dab65a01d2ce8b47c328351d7
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-10 15:25:03 -0700

    Update step01

---
+++ step01_variables_and_types/app.ts
@@ -0,0 +1,5 @@
+let itemCount: number = 10;
+itemCount = 15; // Valid - Changing the value of a mutable variable
+
+const maxItems: number = 100;
+// maxItems = 120; // Invalid - Attempting to change the value of a constant variable
