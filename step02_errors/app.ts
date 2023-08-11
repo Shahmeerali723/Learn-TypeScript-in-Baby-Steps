34e995a7c9ed4d8c8bf82a654ac7aa91dc03e76b
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-11 13:50:46 -0700

    Update step02

--- step02_errors/app.ts
+++ step02_errors/app.ts
@@ -7,3 +7,26 @@ let y: numer = 20;   // Syntax error: Should be 'number' instead of 'numer'
 
 let name: string = "John";
 name = 10;  // Type error: Cannot assign a number to a string variable
+
+// Undefined or null Errorss:
+
+let person: { name: string } = null;
+console.log(person.name);  // Runtime error: Cannot read property 'name' of null
+
+// Scope Errors:
+
+if (true) {
+    let myVar = 10;
+}
+console.log(myVar);  // Scope error: 'myVar' is not accessible here
+
+// Function Errors:
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+let result: string = add(5, 3);  // Type error: Result should be a number, not a string
+
+// Unsed Variable Error
+
+// Property Assisgnment Errors:
