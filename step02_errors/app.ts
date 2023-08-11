52a971dd73e154282ab13ba2cc03b0cde680c69d
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-11 13:47:44 -0700

    Update step02

--- step02_errors/app.ts
+++ step02_errors/app.ts
@@ -3,3 +3,7 @@
 let x: number = 10;  // Correct
 let y: numer = 20;   // Syntax error: Should be 'number' instead of 'numer'
 
+// Type Errors:
+
+let name: string = "John";
+name = 10;  // Type error: Cannot assign a number to a string variable
